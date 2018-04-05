ea4849bc6e08fbdc61cc10a5f71c9ff14cef387c
Chrystal Zou <chrystalzou@gmail.com>
2018-04-05 22:04:58 +0800

    Revert "Changed assert function to chai.assert"
    This reverts commit 5d4478005fd0f43affd7a0e7d18deaf4f92b8f81.

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -78,7 +78,7 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it. 
   //   http://chaijs.com/
-  var assert = chai.assert;
+  var assert = chai.FILL_ME_IN;
  
 
   it('has a prefix of 4 and a length of 13', function() {
