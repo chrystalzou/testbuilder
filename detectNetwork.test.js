f9be0d1cb54cd6559d0da4fb0ba6732cc80f59c4
Chrystal Zou <chrystalzou@gmail.com>
2018-04-06 13:22:46 +0800

    China UnionPay

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -135,8 +135,6 @@ describe('MasterCard', function() {
 describe('Discover', function() {
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
-  var should = chai.should();
-
   it('has a prefix of 6011 and a length of 16', function() {
     detectNetwork('6011756475867586').should.equal('Discover');
   });
